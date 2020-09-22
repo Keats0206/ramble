2bfff137b79ee2e8baeda1c72cb9278b7775f152
Keats0206 <keats0206@gmail.com>
2020-09-21 20:24:08 -0400

    Floating PLayer build

---
+++ Ramble/View/RambUserCell.swift
@@ -0,0 +1,21 @@
+//
+//  RambUserCell.swift
+//  Ramble
+//
+//  Created by Peter Keating on 9/18/20.
+//  Copyright © 2020 Peter Keating. All rights reserved.
+//
+
+import SwiftUI
+
+struct RambUserCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RambUserCell_Previews: PreviewProvider {
+    static var previews: some View {
+        RambUserCell()
+    }
+}
