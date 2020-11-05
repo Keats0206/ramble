1fbf8f5af5427eab95767cdc8d36573f4aab9249
Pete Keating <pete@pekeating.com>
2020-11-05 11:42:47 -0500

    player view dragging

--- Ramble/View/NowPlayingView.swift
+++ Ramble/View/NowPlayingView.swift
@@ -10,13 +10,17 @@ import SwiftUI
 import SDWebImageSwiftUI
 
 @available(iOS 14.0, *)
-
 struct NowPlayingBar<Content: View>: View {
     @EnvironmentObject var globalPlayer: GlobalPlayer
+   
     @Namespace private var expandAnimation
+    
     @State private var isExpanded = false
     @State var volume = 50.0
     @State private var isActive = false
+    
+    @State var height : CGFloat = 0
+    @State var floating = true
     
     var ramb: Ramb2 = testRamb
     
@@ -25,11 +29,9 @@ struct NowPlayingBar<Content: View>: View {
     var imageFrame: CGFloat {
         isExpanded ? screenBounds.width * 0.7 : 48
     }
-    
     var playFrame: CGFloat {
         isExpanded ? 65 : 25
     }
-    
     var cornerRadius: CGFloat {
         isExpanded ? 20 : 0
     }
@@ -37,157 +39,203 @@ struct NowPlayingBar<Content: View>: View {
     var content: Content
     
     @ViewBuilder var body: some View {
-        
-        ZStack(alignment: .bottom) {
-            content
-            
-            VStack{
+            ZStack(alignment: .bottom) {
+                content
                 
-                Spacer()
-                
-                ZStack(alignment: .top) {
-            
-                    Color.white
-                        .opacity(0.0)
-                        .frame(height: isExpanded ? screenBounds.height - 100 : 80)
-                        .clipShape(CornerShape(corner: [.topLeft, .topRight], size: CGSize(width: cornerRadius, height: cornerRadius)))
-                        .background(Blur())
+                VStack {
+                    
+                    Spacer()
+                    
+                    GeometryReader { geo in
+                        
+                        ZStack(alignment: .top) {
+                    
+                            Color.white
+                                .edgesIgnoringSafeArea(.top)
+                                .opacity(0.0)
+                                .frame(height: isExpanded ? screenBounds.height + 60 : 80)
+                                .clipShape(CornerShape(corner: [.topLeft, .topRight], size: CGSize(width: cornerRadius, height: cornerRadius)))
+                                .background(Blur())
 
-                    VStack {
-                        
-                        HStack {
-                            if #available(iOS 14.0, *) {
-                                WebImage(url: URL(string: "\(ramb.user.profileImageUrl)"))
-                                    .resizable()
-                                    .scaleEffect()
-                                    .frame(width: imageFrame, height: imageFrame)
-                                    .matchedGeometryEffect(id: "AlbumImage", in: expandAnimation)
-                                    .clipShape(Rectangle())
-                                    .cornerRadius(8)
-                                
-                            } else {
-                                // Fallback on earlier versions
-                                WebImage(url: URL(string: "\(ramb.user.profileImageUrl)"))
-                                    .resizable()
-                                    .frame(width: imageFrame, height: imageFrame)
-                                    .clipShape(Rectangle())
-                                    .cornerRadius(8)
-                            }
-                            
-                            if !isExpanded {
-                                
-                                VStack(alignment: .leading) {
-                                    
-                                    Text("@\(ramb.user.username)")
-                                        .font(.system(.caption, design: .rounded))
-                                        .matchedGeometryEffect(id: "Username", in: expandAnimation)
-                                    
-                                    Text("\(ramb.caption)")
-                                        .font(.system(.body, design: .rounded))
-                                        .bold()
-                                        .matchedGeometryEffect(id: "Caption", in: expandAnimation)
-                                }
-                                
-                                Spacer()
-                                
-                                Button(action: {
-                                    print("Fuck YEA")
-                                }){
-                                    Image(systemName: "play.fill")
-                                        .resizable()
-                                        .foregroundColor(.primary)
-                                        .frame(width: playFrame, height: playFrame)
-                                }
-                            }
-                            
-                        }
-                        
-                        if isExpanded {
                             VStack {
-                                ZStack {
-//                                    NavigationLink(destination: ProfileView(user: .constant(ramb.user)), isActive: $isActive)
-//                                    { EmptyView() }.frame(width: 0, height: 0)
-                                    VStack(alignment: .leading) {
-                                        Text("@\(ramb.user.username)")
-                                            .font(.system(.title, design: .rounded))
-                                            .matchedGeometryEffect(id: "Username", in: expandAnimation)
-                                        Text("\(ramb.caption)")
-                                            .font(.system(.body, design: .rounded))
-                                            .bold()
-                                            .matchedGeometryEffect(id: "Caption", in: expandAnimation)
-                                    }
-                                }
-                                                                                            
-                                Spacer()
-                                
-                                VStack {
-                                    Rectangle()
-                                        .frame(height: 3)
-                                        .cornerRadius(3)
-                                        .foregroundColor(Color.accent4)
-                                    HStack {
-                                        Text("0:00")
-                                            .font(.caption)
-                                        Spacer()
-                                        Text("-3:26")
-                                            .font(.caption)
-                                    }
-                                }.foregroundColor(Color.primary)
-                                
-                                Spacer()
                                 
                                 HStack {
-                                    Button(action: {
-                                        print("Fuck YEA")
-                                    }){
-                                        Image(systemName: "backward.fill")
-                                            .font(.system(size: 20))
+                                    WebImage(url: URL(string: "\(ramb.user.profileImageUrl)"))
+                                        .resizable()
+                                        .scaleEffect()
+                                        .frame(width: imageFrame, height: imageFrame)
+                                        .matchedGeometryEffect(id: "AlbumImage", in: expandAnimation)
+                                        .clipShape(Rectangle())
+                                        .cornerRadius(8)
+                                                                    
+                                    if !isExpanded {
+                                        
+                                        VStack(alignment: .leading) {
+                                            
+                                            Text("@\(ramb.user.username)")
+                                                .font(.system(.caption, design: .rounded))
+                                                .matchedGeometryEffect(id: "Username", in: expandAnimation)
+                                            
+                                            Text("\(ramb.caption)")
+                                                .font(.system(.body, design: .rounded))
+                                                .bold()
+                                                .matchedGeometryEffect(id: "Caption", in: expandAnimation)
+                                        }
+                                        
+                                        Spacer()
+                                        
+                                        Button(action: {
+                                            print("Fuck YEA")
+                                        }){
+                                            Image(systemName: "play.fill")
+                                                .resizable()
+                                                .foregroundColor(.primary)
+                                                .frame(width: playFrame, height: playFrame)
+                                        }
                                     }
                                     
-                                    Spacer()
-                                  
-                                    Button(action: {
-                                        print("Fuck YEA")
-                                    }){
-                                        Image(systemName: "play.fill")
-                                            .font(.system(size: 36))
-                                    }
-                                    
-                                    Spacer()
-                                    
-                                    Button(action: {
-                                        print("Fuck YEA")
-                                    }){
-                                        Image(systemName: "forward.fill")
-                                            .font(.system(size: 24))
-                                    }
-                                }.foregroundColor(.primary)
-                                
-                                Spacer()
-                                
-                                // Volume
-                                HStack(spacing: 32) {
-                                    Image(systemName: "volume.fill")
-                                    Slider(value: $volume, in: 0...100)
-                                        .accentColor(Color.secondary)
-                                    Image(systemName: "volume.3.fill")
                                 }
                                 
-                                Spacer()
-                                    
-                            }.padding(.top, 20)
-                            .padding(.horizontal, 40)
+                                if isExpanded {
+                                    VStack {
+                                        
+                                        ZStack {
+        //                                    NavigationLink(destination: ProfileView(user: .constant(ramb.user)), isActive: $isActive)
+        //                                    { EmptyView() }.frame(width: 0, height: 0)
+                                            
+                                                VStack(alignment: .leading) {
+                                                    Text("@\(ramb.user.username)")
+                                                        .font(.system(.title, design: .rounded))
+                                                        .matchedGeometryEffect(id: "Username", in: expandAnimation)
+                                                    Text("\(ramb.caption)")
+                                                        .font(.system(.body, design: .rounded))
+                                                        .bold()
+                                                        .matchedGeometryEffect(id: "Caption", in: expandAnimation)
+                                                }
+                                                VStack(alignment: .leading) {
+                                                    Text("@\(ramb.user.username)")
+                                                        .font(.system(.title, design: .rounded))
+                                                    Text("\(ramb.caption)")
+                                                        .font(.system(.body, design: .rounded))
+                                                        .bold()
+                                                }
+                                        }
+                                                                                                    
+                                        Spacer()
+                                        
+                                        VStack {
+                                            Rectangle()
+                                                .frame(height: 3)
+                                                .cornerRadius(3)
+                                                .foregroundColor(Color.accent4)
+                                            HStack {
+                                                Text("0:00")
+                                                    .font(.caption)
+                                                Spacer()
+                                                Text("-3:26")
+                                                    .font(.caption)
+                                            }
+                                        }.foregroundColor(Color.primary)
+                                        
+                                        Spacer()
+                                        
+                                        HStack {
+                                            Button(action: {
+                                                print("Fuck YEA")
+                                            }){
+                                                Image(systemName: "backward.fill")
+                                                    .font(.system(size: 20))
+                                            }
+                                            
+                                            Spacer()
+                                          
+                                            Button(action: {
+                                                print("Fuck YEA")
+                                            }){
+                                                Image(systemName: "play.fill")
+                                                    .font(.system(size: 36))
+                                            }
+                                            
+                                            Spacer()
+                                            
+                                            Button(action: {
+                                                print("Fuck YEA")
+                                            }){
+                                                Image(systemName: "forward.fill")
+                                                    .font(.system(size: 24))
+                                            }
+                                        }.foregroundColor(.primary)
+                                        
+                                        Spacer()
+                                        
+                                        // Volume
+                                        HStack(spacing: 32) {
+                                            Image(systemName: "volume.fill")
+                                            Slider(value: $volume, in: 0...100)
+                                                .accentColor(Color.secondary)
+                                            Image(systemName: "volume.3.fill")
+                                        }
+                                        
+                                        Spacer()
+                                        
+                                        Spacer()
+                                            
+                                    }.padding(.vertical, 40)
+                                    .padding(.horizontal, 40)
+                                }
+                            }
+                            .padding(.top, isExpanded ? 80 : 16)
+                            .padding(.horizontal)
                         }
-                    }
-                    .padding(.top, isExpanded ? 55 : 16)
-                    .padding(.horizontal)
-                }
-//                .padding(.top, isExpanded ? 50 : 0)
-                .frame(maxHeight: isExpanded ? screenBounds.height - 60 : 80)
-                .onTapGesture {
-                    withAnimation(.spring()) {
-                        isExpanded.toggle()
-                    }
+                        .padding(.top, isExpanded ? 50 : 0)
+                        .frame(maxHeight: isExpanded ? screenBounds.height - 120 : 80)
+                        .onTapGesture {
+                            if self.floating {
+                                withAnimation(.spring()) {
+                                    isExpanded.toggle()
+                                    self.height = 0
+                                    self.floating = false
+                                }
+                            }
+                        }
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ (value) in
+                                    if self.height < geo.size.height - 150 {
+                                        self.height += value.translation.height / 8
+
+                                    }
+                                })
+                                .onEnded({ (_) in
+                                    if self.height > 100 && !self.floating {
+                                        self.height = geo.size.height - 75
+                                        self.floating = true
+                                        self.isExpanded = false
+                                        print("view A")
+                                        print(floating)
+                                        print(height)
+                                    } else {
+                                        if self.height < geo.size.height - 150 {
+                                            self.height = 0
+                                            self.floating = false
+                                            self.isExpanded = true
+                                            print("view B")
+                                            print(floating)
+                                            print(height)
+                                        } else {
+                                            self.height = geo.size.height - 75
+                                            print("view C")
+                                            print(floating)
+                                            print(height)
+                                        }
+                                    }
+                                })
+                            )
+                        .onAppear {
+                            self.height = geo.size.height - 75
+                        }
+                        .offset(y: self.height)
+                        .animation(.spring())
                 }
             }
         }
